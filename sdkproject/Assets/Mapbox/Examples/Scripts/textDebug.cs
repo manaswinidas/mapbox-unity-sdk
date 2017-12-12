d3defac49804f818b51966203611c3d9556cd10b
jim-martin <jim.martin@mapbox.com>
2017-12-12 10:08:44 -0800

    fixed voxel example info window

---
+++ sdkproject/Assets/Mapbox/Examples/Scripts/textDebug.cs
@@ -0,0 +1,20 @@
+﻿namespace Mapbox.Examples
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public class textDebug : MonoBehaviour
+	{
+
+		Text _text;
+
+		void Start()
+		{
+
+			_text = GetComponent<Text>();
+
+			//Debug.Log(_text.text);
+
+		}
+	}
+}
